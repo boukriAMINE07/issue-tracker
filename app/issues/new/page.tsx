ebfaa3b59ccbd1b6a0d790cd14a89053814e22d5
Boukri Amine <AMINE_BOUKRI@emsi-edu.ma>
2023-10-31 15:07:29 +0100

    extract the error message component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,14 +10,15 @@ import {IoInformationCircle} from "react-icons/io5";
 import {createIssueSchema} from "@/app/validation/validationSchema";
 import {zodResolver} from "@hookform/resolvers/zod"
 import {z} from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
 
 
-type IssueForm=z.infer<typeof createIssueSchema>
+type IssueForm = z.infer<typeof createIssueSchema>
 const NewIssue = () => {
     const [error, setError] = useState('');
 
     const router = useRouter();
-    const {register, control, handleSubmit,formState:{errors}} = useForm<IssueForm>({
+    const {register, control, handleSubmit, formState: {errors}} = useForm<IssueForm>({
         resolver: zodResolver(createIssueSchema)
     });
 
@@ -47,17 +48,18 @@ const NewIssue = () => {
                 })}
             >
                 <TextField.Root>
-                    <TextField.Input  placeholder="Title"  {...register("title")} />
+                    <TextField.Input placeholder="Title"  {...register("title")} />
                 </TextField.Root>
-                {errors.title && <Text color="red" as="p" >{errors.title.message}</Text>}
+                <ErrorMessage>{errors.title?.message}</ErrorMessage>
                 <Controller
                     name="description"
                     control={control}
                     render={({field}) => (
-                        <SimpleMDE   placeholder="Description" {...field} />
+                        <SimpleMDE placeholder="Description" {...field} />
                     )}
                 />
-                {errors.description && <Text color="red"  as="p">{errors.description.message}</Text>}
+
+                <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
                 <Button className="cursor-pointer ">Submit New Issue</Button>
             </form>
